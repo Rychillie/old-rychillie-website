397e83088e66e56e9812645862fdb2c36ee22efe
Rychillie <rychillie@hotmail.com>
2021-10-27 14:00:33 -0300

    fit content

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,13 +11,13 @@ type Props = {
 
 const variants = {
   hidden: {
-    height: 0,
+    height: "fit-content",
     opacity: 0,
     x: -200,
     y: 0,
   },
   enter: {
-    height: 0,
+    height: "fit-content",
     opacity: 1,
     x: 0,
     y: 0,
