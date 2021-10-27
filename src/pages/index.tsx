d771dbd8151a97150f8431edb7a5ec9480b1ae18
Rychillie <rychillie@hotmail.com>
2021-10-27 13:42:45 -0300

    trying motion div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
+import { AnimatePresence, motion } from "framer-motion";
 
 import Link from "@components/NoScrollLink";
 import Layout from "@components/Layout";
@@ -19,22 +20,29 @@ const Home: NextPage = () => {
         <Link href="/portfolio">my work </Link> and a little more{" "}
         <Link href="/sobre">about me</Link>.
       </p>
-      <div className={styles.profile}>
-        <div className={styles.containerImage}>
-          <Image
-            src="/images/rychillie.png"
-            blurDataURL="/images/rychillie.png"
-            placeholder="blur"
-            quality={100}
-            layout="fixed"
-            width={48}
-            height={48}
-            alt="this is me"
-            className={styles.profileImage}
-          />
-        </div>
-        <SocialList />
-      </div>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className={styles.profile}
+        >
+          <div className={styles.containerImage}>
+            <Image
+              src="/images/rychillie.png"
+              blurDataURL="/images/rychillie.png"
+              placeholder="blur"
+              quality={100}
+              layout="fixed"
+              width={48}
+              height={48}
+              alt="this is me"
+              className={styles.profileImage}
+            />
+          </div>
+          <SocialList />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 };
