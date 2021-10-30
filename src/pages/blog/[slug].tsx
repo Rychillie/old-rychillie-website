1ab738b8b8b4af33f3f15fb9ded239395c4703af
Rychillie <rychillie@hotmail.com>
2021-10-30 18:25:27 -0300

    só falta o ingles

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,7 +1,8 @@
-import { GetStaticPropsContext } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import { getPostBySlug, getAllPosts } from "@lib/posts";
 import { useRouter } from "next/router";
 import Layout from "@components/Layout";
+import { Params } from "next/dist/server/router";
 
 type Props = {
   locale: "en-US" | string;
@@ -20,18 +21,9 @@ type Props = {
   };
 };
 
-export async function getStaticProps({ params, locale }: Props) {
-  const post = await getPostBySlug(params.slug, { locale });
-  return {
-    props: {
-      ...post,
-      slug: params.slug,
-    },
-  };
-}
-
 export async function getStaticPaths({ locale }: Props) {
   const posts = await getAllPosts({ locale });
+
   return {
     paths: posts.map((post) => ({
       params: {
@@ -42,12 +34,29 @@ export async function getStaticPaths({ locale }: Props) {
   };
 }
 
+// getStaticProps for generate blog post page
+export async function getStaticProps({ locale, params }: Props & Params) {
+  const post = await getPostBySlug(params.slug, { locale });
+
+  return {
+    props: {
+      locale,
+      post,
+    },
+  };
+}
+
 // component for generate blog post page
 const BlogPost = ({ post, locale }: Props) => {
   console.log(post);
   return (
-    <Layout title={"title"} description={"description"} hasHeader hasFooter>
-      <h1>title</h1>
+    <Layout
+      title={post.title}
+      description={post.description}
+      hasHeader
+      hasFooter
+    >
+      <h1>{post.title}</h1>
     </Layout>
   );
 };
