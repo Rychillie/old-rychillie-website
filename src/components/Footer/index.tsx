e4c51943e75f19b9df13d010072149bcc420853b
Rychillie <rychillie@hotmail.com>
2021-11-02 18:45:32 -0300

    👌 IMPROVE: Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,52 @@
 import { AnimateSharedLayout } from "framer-motion";
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import content from "../../data/components.json";
 
-const Footer = (): JSX.Element => {
-  return <footer className={styles.footer}></footer>;
+type Props = {
+  locale?: string | undefined;
+};
+
+const Footer = ({ locale }: Props): JSX.Element => {
+  const localeContent = locale === "pt-BR" ? "pt-BR" : "en-US";
+  const { pages, social, extra } = content.footer[localeContent];
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.side}>
+        <h2>
+          <Link href="/">
+            <a>
+              🦄 <span>Rychillie</span>
+            </a>
+          </Link>
+        </h2>
+      </div>
+      <div className={styles.side}>
+        <div className={styles.list}>
+          {pages.map(({ name, href }) => (
+            <Link key={name} href={href}>
+              <a>{name}</a>
+            </Link>
+          ))}
+        </div>
+        <div className={styles.list}>
+          {social.map(({ name, href }) => (
+            <a key={name} href={href} target="_blank" rel="noreferrer">
+              {name}
+            </a>
+          ))}
+        </div>
+        <div className={styles.list}>
+          {extra.map(({ name, href }) => (
+            <Link key={name} href={href}>
+              <a>{name}</a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
