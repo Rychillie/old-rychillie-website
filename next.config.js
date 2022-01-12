21a4476cc70a67123317e344b3c62c248e16315c
Rychillie <rychillie@hotmail.com>
2022-01-11 22:12:27 -0300

    👌 IMPROVE:  PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    register: true,
-    skipWaiting: true,
+    runtimeCaching,
   },
 });
 
