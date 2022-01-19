0e96a8f09176d7d3663eef594c89123073b5bbe7
Rychillie <rychillie@hotmail.com>
2022-01-18 21:12:30 -0300

    👌 IMPROVE: Test PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,59 +8,8 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
 
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, []);
-
   return (
     <>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/images/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/images/icon-512x512.png"></link>
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#151718" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-        />
-      </Head>
       <NavigationTop />
 
       <main>
