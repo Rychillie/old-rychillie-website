d18d7c0da83f805260769e809c77059ec56fe3d0
Rychillie <rychillie@hotmail.com>
2022-01-18 21:18:21 -0300

    ‼️ BREAKING: Revert

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,16 +6,62 @@ import Document, {
   DocumentContext,
 } from "next/document";
 
+const APP_NAME = "Rychillie 🦄";
+const APP_DESCRIPTION = "UI Designer, Frontend & IOS Developer";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return await Document.getInitialProps(ctx);
   }
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="pt-BR" dir="ltr">
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#151718" />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/images/safari-pinned-tab.svg"
+            color="#000000"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/images/icon-512x512.png"></link>
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#151718" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
